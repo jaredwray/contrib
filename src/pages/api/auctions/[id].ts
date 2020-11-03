3c2ba544ef443efe0ec975881f0f8a6c562461db
Damien Guard <damieng@gmail.com>
2020-11-03 11:46:25 +0000

    Fix auction mongodb access.

--- src/pages/api/auctions/[id].ts
+++ src/pages/api/auctions/[id].ts
@@ -1,23 +1,21 @@
-import { ObjectID } from 'mongodb';
-import { NextApiRequest, NextApiResponse } from 'next';
-import { connectToDatabase } from '../../../util/mongodb';
+import { ObjectID } from 'mongodb'
+import { NextApiRequest, NextApiResponse } from 'next'
+import { connectToDatabase } from '../../../../utils/mongodb'
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     if (req.method !== 'GET') {
-        res.setHeader('Allow', 'GET');
-        res.status(405).end('Method Not Allowed');
+        res.setHeader('Allow', 'GET')
+        res.status(405).end('Method Not Allowed')
     } else {
         const {
             query: { id }
-        } = req;
+        } = req
         try {
-            const { docs } = await connectToDatabase();
-
+            const { docs } = await connectToDatabase()
             const auction = await docs.auctions().findOne({_id: new ObjectID(id.toString())})
-
-            res.status(200).json(auction);
+            res.status(200).json(auction)
         } catch (err) {
-            res.status(500).json({ statusCode: 500, message: err.message });
+            res.status(500).json({ statusCode: 500, message: err.message })
         }
     }
 }
--- src/pages/api/auctions/index.ts
+++ src/pages/api/auctions/index.ts
@@ -1,22 +1,20 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-import { connectToDatabase } from '../../../util/mongodb';
+import { NextApiRequest, NextApiResponse } from 'next'
+import { connectToDatabase } from '../../../../utils/mongodb'
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     if (req.method !== 'GET') {
-        res.setHeader('Allow', 'GET');
-        res.status(405).end('Method Not Allowed');
+        res.setHeader('Allow', 'GET')
+        res.status(405).end('Method Not Allowed')
     } else {
         try {
-            const { docs } = await connectToDatabase();
-
+            const { docs } = await connectToDatabase()
             const auctions = await docs.auctions()
                 .find({})
                 .limit(20)
-                .toArray();
-
-            res.status(200).json({ auctions });
+                .toArray()
+            res.status(200).json({ auctions })
         } catch (err) {
-            res.status(500).json({ statusCode: 500, message: err.message });
+            res.status(500).json({ statusCode: 500, message: err.message })
         }
     }
 }
