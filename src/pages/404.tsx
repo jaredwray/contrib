24f95dddefffc426197bc4f99473fff70a372288
Damien Guard <damieng@gmail.com>
2020-11-09 14:03:48 +0000

    Tweak 404, try TSX for templates.

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,11 +1,6 @@
 import React from 'react'
-
 import Link from 'next/link'
-
-import {
-    Container,
-    Button
-} from 'reactstrap'
+import { Container, Button } from 'reactstrap'
 
 export async function getStaticProps() {
     return {
@@ -27,11 +22,12 @@ const Error404 = () => {
                 <img src="/content/img/photo/aron-visuals-3jBU9TbKW7o-unsplash.jpg" alt="Not found" className="bg-image" />
                 <Container className="text-white text-lg overlay-content py-6 py-lg-0">
                     <h1 className="display-3 font-weight-bold mb-5">Oops, that page is not here.</h1>
-                    <p className="font-weight-light mb-5">Elit adipisicing aliquip irure non consequat quis ex fugiat dolor in irure pariatur eu aute. Ea tempor nisi sit in Lorem exercitation mollit ut veniam in do eu excepteur. </p>
+                    <p className="font-weight-light mb-5">Sorry we're still building out the site right now so it's possible there should have beens something here.</p>
+                    <p className="font-weight-light mb-5">Then again, maybe not.</p>
                     <p className="mb-6">
                         <Link href="index">
                             <Button href="index" color="outline-light">
-                                <i className="fa fa-home mr-2" />Start from the Homepage
+                                <i className="fa fa-home mr-2" />Head Home
                             </Button>
                         </Link></p>
                     <p className="h4 text-shadow">Error 404</p>
