47d5b1c555f2fee2e13b95b7b171179cd4eaf360
Damien Guard <damieng@gmail.com>
2020-11-22 12:20:13 +0000

    Debug api endpoint for database - precursor to populator

---
+++ src/pages/api/debug/database.ts
@@ -0,0 +1,16 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import { connectToDatabase } from '../../../../utils/mongodb'
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  const { docs } = await connectToDatabase()
+
+  const collections = (await docs._db.listCollections().toArray()).map(c => ({
+    name: c.name,
+    count: 0
+  }))
+
+  for(const collection of collections)
+    collection.count = await docs._db.collection(collection.name).count()
+
+  res.status(200).json(collections)
+}
