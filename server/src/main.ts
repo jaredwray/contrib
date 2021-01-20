8ece4f264dc68a44c7a1932e0f8c1a1eef7e111f
Jared Wray <me@jaredwray.com>
2021-01-20 10:12:14 -0800

    NewRelic APM and Codecov

--- server/src/main.ts
+++ server/src/main.ts
@@ -2,6 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+
+  if(process.env.NEWRELIC_LICENSE_KEY !== undefined && process.env.NEW_RELIC_APP_NAME !== undefined) {
+    require("newrelic");
+  }
+
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
