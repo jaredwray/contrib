99eacdfc36fb4bd237d9000e8aaf3827629a37c4
Yury Bobkov <yury.bobkov@aterise.com>
2021-03-08 13:28:03 +0300

    create form component and inputs

---
+++ client/src/components/Form/SwitchField/index.tsx
@@ -0,0 +1,38 @@
+import React, { FC } from 'react';
+
+import { Form as BsForm } from 'react-bootstrap';
+
+import useField from '../hooks/useField';
+import useFieldConstraints from '../hooks/useFieldConstraints';
+
+import './styles.scss';
+
+const { Switch } = BsForm;
+
+interface Props {
+  name: string;
+  title: string;
+  required?: boolean;
+  disabled?: boolean;
+  className?: string;
+  constraints?: { [key: string]: any };
+}
+
+const SwitchField: FC<Props> = ({ name, title, required, disabled, className, constraints: inputConstraints }) => {
+  const constraints = useFieldConstraints(inputConstraints, required);
+  const { hasError, errorMessage, value, ...inputProps } = useField(name, { constraints, disabled, type: 'checkbox' });
+
+  return (
+    <Switch
+      custom
+      checked={value}
+      id={name}
+      label={title}
+      {...inputProps}
+      bsCustomPrefix="custom-switch2"
+      className={className}
+    />
+  );
+};
+
+export default SwitchField;
