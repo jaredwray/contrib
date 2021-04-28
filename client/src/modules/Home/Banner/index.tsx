a7df174ddd70eeeb046423c5ef8709a075003951
Yauheni Shydlouski <yauheni.shydlouski@aterise.com>
2021-04-28 20:04:28 +0300

    BG picture on the homepage

--- client/src/modules/Home/Banner/index.tsx
+++ client/src/modules/Home/Banner/index.tsx
@@ -9,28 +9,30 @@ export default function Banner() {
   return (
     <section className={styles.banner}>
       <Container className={clsx(styles.homepageContainer, 'position-relative h-100')} fluid="sm">
-        <Row>
-          <Col className="pt-5 pt-md-4 pt-lg-5 pb-3 pb-md-4 pb-lg-4 text-super" lg="8" xs="6">
-            Make An Impact
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            <div className={styles.separator} />
-          </Col>
-        </Row>
-        <Row>
-          <Col className="pt-3 pb-3 text-headline" lg="8" xs="12">
-            Auction your items quickly and hassle free
-          </Col>
-        </Row>
-        <Row>
-          <Col className={clsx(styles.signature, 'text-label text-all-cups position-absolute')} sm="6" xs="9">
-            Stephan Frei
-            <br />
-            Total raised: $248,000
-          </Col>
-        </Row>
+        <div className={styles.wrapper}>
+          <Row>
+            <Col className="pt-5 pt-md-4 pt-lg-5 pb-3 pb-md-4 pb-lg-4 text-super" lg="8" xs="6">
+              Make An Impact
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <div className={styles.separator} />
+            </Col>
+          </Row>
+          <Row>
+            <Col className="pt-3 pb-3 text-headline" lg="6">
+              Auction your items quickly and hassle free
+            </Col>
+          </Row>
+          <Row>
+            <Col className={clsx(styles.signature, 'text-label text-all-cups position-absolute')} sm="6" xs="9">
+              Stephan Frei
+              <br />
+              Total raised: $248,000
+            </Col>
+          </Row>
+        </div>
       </Container>
     </section>
   );
