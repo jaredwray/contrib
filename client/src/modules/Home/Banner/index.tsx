79a29168241d2db673ba75cd0ce543b72978fa52
Yauheni Shydlouski <yauheni.shydlouski@aterise.com>
2021-04-28 18:13:44 +0300

    Banner fixes

--- client/src/modules/Home/Banner/index.tsx
+++ client/src/modules/Home/Banner/index.tsx
@@ -1,14 +1,11 @@
 import React from 'react';
 
-import { useAuth0 } from '@auth0/auth0-react';
 import clsx from 'clsx';
-import { Container, Row, Col, Button } from 'react-bootstrap';
+import { Container, Row, Col } from 'react-bootstrap';
 
 import styles from './styles.module.scss';
 
 export default function Banner() {
-  const { isAuthenticated, loginWithRedirect } = useAuth0();
-
   return (
     <section className={styles.banner}>
       <Container className={clsx(styles.homepageContainer, 'position-relative h-100')} fluid="sm">
@@ -27,19 +24,6 @@ export default function Banner() {
             Auction your items quickly and hassle free
           </Col>
         </Row>
-        {!isAuthenticated && (
-          <Row>
-            <Col xs="6">
-              <Button
-                className={clsx(styles.button, 'btn-with-arrows text-label')}
-                variant="secondary"
-                onClick={() => loginWithRedirect({ page_type: 'sign_up' })}
-              >
-                Sign Up
-              </Button>
-            </Col>
-          </Row>
-        )}
         <Row>
           <Col className={clsx(styles.signature, 'text-label text-all-cups position-absolute')} sm="6" xs="9">
             Stephan Frei
