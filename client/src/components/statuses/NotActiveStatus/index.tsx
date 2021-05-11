4c85d75eca63ccb9f1b5fd6f96366e2df35adec2
Yauheni Shydlouski <yauheni.shydlouski@aterise.com>
2021-05-11 03:12:44 +0300

    NotActiveStatus badge text fix

--- client/src/components/statuses/NotActiveStatus/index.tsx
+++ client/src/components/statuses/NotActiveStatus/index.tsx
@@ -7,7 +7,7 @@ import styles from './styles.module.scss';
 const NotActiveStatus: FC = () => (
   <div className="text-label d-flex align-items-center mb-2">
     <InactiveIcon className="mr-2" />
-    <span className={styles.root}>Inactive Charity</span>
+    <span className={styles.root}>Not Active Charity</span>
   </div>
 );
 
