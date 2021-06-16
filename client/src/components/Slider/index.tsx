d431202f837db1c500e7a737ab2a9eeb136d4f8d
Yauheni Shydlouski <yauheni.shydlouski@aterise.com>
2021-06-16 13:35:48 +0300

    responsive Slider

--- client/src/components/Slider/index.tsx
+++ client/src/components/Slider/index.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react';
+import { FC, useState, useEffect } from 'react';
 
 import RSlider from 'react-slick';
 
@@ -9,10 +9,25 @@ interface Props {
 }
 
 const Slider: FC<Props> = ({ items }) => {
+  function getWindowWidth() {
+    const { innerWidth: width } = window;
+    return width;
+  }
+  const [windowWidth, setWindowWidth] = useState(getWindowWidth());
+
+  useEffect(() => {
+    function handleResize() {
+      setWindowWidth(getWindowWidth());
+    }
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
   const settings = {
     infinite: true,
     swipeToSlide: true,
     variableWidth: true,
+    slidesToShow: items.length <= Math.round(windowWidth / 500) ? items.length : 1,
   };
 
   if (!items.length) {
