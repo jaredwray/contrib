edd2becf2b23b6c851bdcca8f6ff47275de1bf21
Yauheni Shydlouski <yauheni.shydlouski@aterise.com>
2021-07-08 17:16:35 +0300

    tests

--- client/src/modules/Influencer/Onboarding/__test__/InfluencerOnboardingBasicPage.test.tsx
+++ client/src/modules/Influencer/Onboarding/__test__/InfluencerOnboardingBasicPage.test.tsx
@@ -1,38 +1,94 @@
 import { InMemoryCache } from '@apollo/client';
 import { MockedProvider } from '@apollo/client/testing';
-import { render } from '@testing-library/react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { mount, ReactWrapper } from 'enzyme';
+import { act } from 'react-dom/test-utils';
 import { ToastProvider } from 'react-toast-notifications';
-
-import { MyAccountQuery } from 'src/apollo/queries/accountQuery';
-
+import { MemoryRouter } from 'react-router-dom';
+import Layout from 'src/components/Layout';
 import { InfluencerOnboardingBasicPage } from '../InfluencerOnboardingBasicPage';
+import { UserAccountStatus } from 'src/types/UserAccount';
+import { MyAccountQuery } from 'src/apollo/queries/accountQuery';
+import Form from 'src/components/Form/Form';
 
 jest.mock('src/components/TermsConfirmationDialog', () => () => <></>);
 
-const props: any = {
-  name: 'test name',
-  sport: 'soccer',
-  team: 'MU',
-  profileDescription: 'test',
-};
-
 const cache = new InMemoryCache();
+
+const mockHistoryPush = jest.fn();
+
+jest.mock('react-router-dom', () => ({
+  ...jest.requireActual('react-router-dom'),
+  useHistory: () => ({
+    push: mockHistoryPush,
+  }),
+}));
+
 cache.writeQuery({
   query: MyAccountQuery,
   data: {
-    myAccount: {},
+    myAccount: {
+      id: '123',
+      phoneNumber: '123',
+      status: UserAccountStatus.PHONE_NUMBER_CONFIRMATION_REQUIRED,
+      influencerProfile: {
+        avatarUrl: 'test',
+        id: 'test',
+        name: 'test',
+        profileDescription: '1',
+        sport: '1',
+        status: 'TRANSIENT',
+        team: '1',
+      },
+      isAdmin: false,
+      createdAt: '2021-02-18T14:36:35.208+00:00',
+      notAcceptedTerms: null,
+      assistant: null,
+      paymentInformation: null,
+      mongodbId: '321',
+      charity: null,
+    },
   },
 });
-
-test('renders without crashing', () => {
-  render(
-    <Router>
-      <ToastProvider>
-        <MockedProvider cache={cache}>
-          <InfluencerOnboardingBasicPage {...props} />
-        </MockedProvider>
-      </ToastProvider>
-    </Router>,
-  );
+describe('InfluencerOnboardingBasicPage ', () => {
+  it('component return null', async () => {
+    let wrapper: ReactWrapper;
+    await act(async () => {
+      wrapper = mount(
+        <MemoryRouter>
+          <ToastProvider>
+            <MockedProvider>
+              <InfluencerOnboardingBasicPage />
+            </MockedProvider>
+          </ToastProvider>
+        </MemoryRouter>,
+      );
+    });
+    await act(async () => {
+      await new Promise((resolve) => setTimeout(resolve));
+      wrapper.update();
+    });
+    expect(wrapper!.find(Layout)).toHaveLength(0);
+  });
+  it('component is defined and have Layout', async () => {
+    let wrapper: ReactWrapper;
+    await act(async () => {
+      wrapper = mount(
+        <MemoryRouter>
+          <ToastProvider>
+            <MockedProvider cache={cache}>
+              <InfluencerOnboardingBasicPage />
+            </MockedProvider>
+          </ToastProvider>
+        </MemoryRouter>,
+      );
+    });
+    await act(async () => {
+      await new Promise((resolve) => setTimeout(resolve));
+      wrapper.update();
+    });
+    expect(wrapper!).toHaveLength(1);
+    expect(wrapper!.find(Layout)).toHaveLength(1);
+    wrapper!.find(Form).simulate('submit');
+    expect(mockHistoryPush).toHaveBeenCalledTimes(0);
+  });
 });
