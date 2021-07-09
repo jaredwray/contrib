f30054736c38d13655a823ccb9c02434fb2adc2e
Yauheni Shydlouski <yauheni.shydlouski@aterise.com>
2021-07-09 17:48:31 +0300

    tests

--- client/src/modules/admin/auctions/AdminAuctionsPage/__tests__/StopOrActiveButton.test.tsx
+++ client/src/modules/admin/auctions/AdminAuctionsPage/__tests__/StopOrActiveButton.test.tsx
@@ -1,34 +1,48 @@
 import { BrowserRouter as Router } from 'react-router-dom';
-import { ToastProvider } from 'react-toast-notifications';
-import { mount } from 'enzyme';
-import { gql } from '@apollo/client';
-import { MockedProvider } from '@apollo/client/testing';
-
 import { StopOrActiveButton } from '../StopOrActiveButton';
 import { auction } from 'src/helpers/testHelpers/auction';
-
-jest.mock('src/components/TermsConfirmationDialog', () => () => <></>);
-
-const props: any = {
-  auction,
-  mutation: gql`
-    mutation test($name: String!) {
-      test(name: $name) {
-        name
+import { mount, ReactWrapper } from 'enzyme';
+import { gql } from '@apollo/client';
+import { MockedProvider } from '@apollo/client/testing';
+import { ToastProvider } from 'react-toast-notifications';
+import { Modal } from 'src/modules/admin/auctions/AdminAuctionsPage/StopOrActiveButton/Modal';
+describe('Should render correctly "StopOrActiveButton"', () => {
+  const props: any = {
+    auction,
+    mutation: gql`
+      mutation test($name: String!) {
+        test(name: $name) {
+          name
+        }
       }
-    }
-  `,
-  className: jest.fn(),
-};
+    `,
+    className: jest.fn(),
+  };
+  const mockFn = jest.fn();
 
-test('renders without crashing', () => {
-  mount(
-    <ToastProvider>
-      <MockedProvider>
-        <Router>
-          <StopOrActiveButton {...props} />
-        </Router>
-      </MockedProvider>
-    </ToastProvider>,
-  );
+  let wrapper: ReactWrapper;
+  beforeEach(() => {
+    wrapper = mount(
+      <ToastProvider>
+        <MockedProvider>
+          <Router>
+            <StopOrActiveButton {...props} />
+          </Router>
+        </MockedProvider>
+      </ToastProvider>,
+    );
+  });
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+  it('component is defined', () => {
+    expect(wrapper).toHaveLength(1);
+  });
+  it('it should open Modal when clicking', () => {
+    wrapper.children().find('Button').simulate('click');
+  });
+  it('it should close Modal when clicking', () => {
+    wrapper.children().find('Button').simulate('click');
+    wrapper.children().find(Modal).children().find('Button').first().simulate('click');
+  });
 });
--- client/src/modules/auctions/editAuction/__test__/DonePage.test.tsx
+++ client/src/modules/auctions/editAuction/__test__/DonePage.test.tsx
@@ -1,41 +1,107 @@
-import { InMemoryCache } from '@apollo/client';
 import { MockedProvider } from '@apollo/client/testing';
-import { render } from '@testing-library/react';
-import { act } from 'react-dom/test-utils';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { mount, ReactWrapper } from 'enzyme';
 import { AuctionQuery } from 'src/apollo/queries/auctions';
+import { MemoryRouter } from 'react-router-dom';
+import { InMemoryCache } from '@apollo/client';
+import Layout from 'src/components/Layout';
+import { act } from 'react-dom/test-utils';
+import AuctionDonePage from 'src/modules/auctions/editAuction/DonePage';
 
-import DonePage from '../DonePage';
+jest.mock('react-router-dom', () => ({
+  ...jest.requireActual('react-router-dom'),
+  useParams: () => ({
+    auctionId: '60d0f0f4f7714a2f8ae247df',
+  }),
+  useRouteMatch: () => ({ url: '/auctions/60d0f0f4f7714a2f8ae247df/done' }),
+}));
 
 jest.mock('src/components/TermsConfirmationDialog', () => () => <></>);
 
 const cache = new InMemoryCache();
+
 cache.writeQuery({
   query: AuctionQuery,
+  variables: { id: '60d0f0f4f7714a2f8ae247df' },
   data: {
     auction: {
-      id: '123',
-      title: 'test title',
-      sport: 'soccer',
-      gameWorn: true,
-      link: 'test/link',
-      autographed: true,
-      authenticityCertificate: true,
-      playedIn: true,
-      description: 'test description',
-      fullPageDescription: 'test fullPageDescription',
+      attachments: [],
+      auctionOrganizer: {
+        id: '602fd93aab319f3eea16a1a6',
+        name: 'Bob',
+        avatarUrl: 'https://storage.googleapis.com/content-dev.contrib.org/602fd93aab319f3eea16a1a6/avatar/avatar.webp',
+      },
+      authenticityCertificate: false,
+      autographed: false,
+      bids: [],
+      charity: {
+        id: '60c1f579ff49a51d6f2ee61b',
+        name: 'My Active Charity Name',
+        avatarUrl: '/content/img/users/person.png',
+        websiteUrl: 'http://google.com',
+      },
+      currentPrice: { amount: 112200, currency: 'USD', precision: 2 },
+      description: 'dd',
+      endDate: '2021-08-29T08:05:21.000Z',
+      fairMarketValue: null,
+      followers: [],
+      fullPageDescription: 'dd',
+      gameWorn: false,
+      id: '60d0f0f4f7714a2f8ae247df',
+      isActive: true,
+      isDraft: false,
+      isFailed: false,
+      isPending: false,
+      isSettled: false,
+      isSold: false,
+      isStopped: false,
+      itemPrice: { amount: 10000, currency: 'USD', precision: 2 },
+      link: 'https://go.contrib.org/3d1Hwvn',
+      playedIn: null,
+      startDate: '2021-06-21T08:05:21.000Z',
+      startPrice: { amount: 33300, currency: 'USD', precision: 2 },
+      status: 'ACTIVE',
+      stoppedAt: null,
+      timeZone: 'PDT',
+      title: 'dd',
+      totalBids: 1,
     },
   },
 });
 
-test('renders without crashing', async () => {
-  await act(async () => {
-    render(
-      <Router>
-        <MockedProvider cache={cache}>
-          <DonePage />
-        </MockedProvider>
-      </Router>,
-    );
+describe('DonePage ', () => {
+  it('component return null', async () => {
+    let wrapper: ReactWrapper;
+    await act(async () => {
+      wrapper = mount(
+        <MemoryRouter>
+          <MockedProvider>
+            <AuctionDonePage />
+          </MockedProvider>
+        </MemoryRouter>,
+      );
+    });
+    await act(async () => {
+      await new Promise((resolve) => setTimeout(resolve));
+      wrapper.update();
+    });
+    expect(wrapper!.find(Layout)).toHaveLength(0);
+  });
+  xit('component is defined and have Layout', async () => {
+    let wrapper: ReactWrapper;
+    await act(async () => {
+      wrapper = mount(
+        <MemoryRouter>
+          <MockedProvider cache={cache}>
+            <AuctionDonePage />
+          </MockedProvider>
+        </MemoryRouter>,
+      );
+    });
+    await act(async () => {
+      await new Promise((resolve) => setTimeout(resolve));
+      wrapper.update();
+    });
+    expect(wrapper!).toHaveLength(1);
+    expect(wrapper!.find(Layout)).toHaveLength(1);
   });
 });
