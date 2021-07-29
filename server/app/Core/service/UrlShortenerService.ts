61180c01c50c4d7764a4813c70f5d1d4d677edc8
Yauheni Shydlouski <yauheni.shydlouski@aterise.com>
2021-07-29 17:15:37 +0300

    bitly import fix

--- server/app/Core/service/UrlShortenerService.ts
+++ server/app/Core/service/UrlShortenerService.ts
@@ -1,5 +1,5 @@
 import { Bitly } from 'bitly';
-import { BitlyMetricsByCountryRes, BitlyMetricsByReferrers } from 'bitly/dist/types';
+import { BitlyTimeUnit } from 'bitly/dist/types';
 import { BitlyClick } from '../../Auction/dto/AuctionMetrics';
 import { AppConfig } from '../../../config';
 import { Metrics } from '../../Auction/dto/AuctionMetrics';
@@ -14,19 +14,16 @@ export class UrlShortenerService {
     return result.link;
   }
 
-  async getMetricsFromLastUpdate(url: string, units: number): Promise<Metrics> {
+  async getMetricsFromLastUpdate(url: string, incomingUnits: number): Promise<Metrics> {
+    const { unit, units } = this.getCurrentUnits(incomingUnits);
     const hash = this.getHashFromUrl(url);
-    const clicks = await this.bitly.clicks(url, 'hour', 1);
-    const referrers: BitlyMetricsByReferrers = await this.bitly.bitlyRequest(
-      `bitlinks/bit.ly/${hash}/referrers`,
-      { unit: 'minute', units },
-      'GET',
-    );
-    const countries: BitlyMetricsByCountryRes = await this.bitly.bitlyRequest(
-      `bitlinks/bit.ly/${hash}/countries`,
-      { unit: 'minute', units },
-      'GET',
-    );
+
+    const requestInput = { unit, units, hash };
+
+    const clicks = await this.getBitlyRequest('clicks', requestInput);
+    const referrers = await this.getBitlyRequest('referrers', requestInput);
+    const countries = await this.getBitlyRequest('countries', requestInput);
+
     return {
       clicks: clicks.link_clicks.filter((value: BitlyClick) => value.clicks > 0),
       referrers: referrers.metrics,
@@ -48,5 +45,36 @@ export class UrlShortenerService {
   private getHashFromUrl(url: string): string {
     const pattern = /\/([A-z0-9_-]{6,})$/;
     return pattern.exec(url)[1];
+  }
+
+  private getCurrentUnits(units: number) {
+    if (units <= 60) {
+      return {
+        unit: 'minute' as BitlyTimeUnit,
+        units,
+      };
+    }
+    const hourUnits = Math.round(units / 60);
+    if (hourUnits <= 60) {
+      return {
+        unit: 'hour' as BitlyTimeUnit,
+        units: hourUnits,
+      };
+    }
+    const dayUnits = Math.round(hourUnits / 24);
+    if (dayUnits <= 60) {
+      return {
+        unit: 'day' as BitlyTimeUnit,
+        units: dayUnits,
+      };
+    }
+  }
+
+  private async getBitlyRequest(
+    type: string,
+    requestInput: { unit: BitlyTimeUnit; units: number; hash: string },
+  ): Promise<any> {
+    const { unit, units, hash } = requestInput;
+    return await this.bitly.bitlyRequest(`bitlinks/bit.ly/${hash}/${type}`, { unit, units }, 'GET');
   }
 }
