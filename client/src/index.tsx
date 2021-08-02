0f3fc71fbedcc5fff394d2c006ad9791e51de654
Yauheni Shydlouski <yauheni.shydlouski@aterise.com>
2021-08-03 02:22:49 +0300

    path to delivery page

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -109,7 +109,7 @@ ReactDOM.render(
                 <PrivateRoute component={NewAuctionWizardPage} path="/auctions/new" role="influencer" />
                 <PrivateRoute component={EditAuctionBasicPage} path="/auctions/:auctionId/basic" role="influencer" />
                 <PrivateRoute component={EditAuctionMediaPage} path="/auctions/:auctionId/media" role="influencer" />
-                <Route component={DeliveryAddressPage} path="/auctions/:auctionId/delivery-address" />
+                <Route component={DeliveryAddressPage} path="/auctions/:auctionId/delivery" />
                 <PrivateRoute
                   component={EditAuctionDetailsPage}
                   path="/auctions/:auctionId/details"
