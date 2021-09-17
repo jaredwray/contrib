25f119ac3c12b63164c2714c1f0c48bc2b224b35
Yauheni Shydlouski <yauheni.shydlouski@aterise.com>
2021-09-18 01:26:46 +0300

    default auction preview

--- client/src/components/AuctionCard/CoverImage/index.tsx
+++ client/src/components/AuctionCard/CoverImage/index.tsx
@@ -1,4 +1,4 @@
-import { FC, useState, useEffect, useRef } from 'react';
+import { FC } from 'react';
 
 import clsx from 'clsx';
 
@@ -12,54 +12,19 @@ type Props = {
   className?: string;
 };
 
-const placeholder =
-  'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkqAcAAIUAgUW0RjgAAAAASUVORK5CYII=';
-
 const CoverImage: FC<Props> = ({ src, alt, className }) => {
-  const imageSrc = src || placeholder;
-
-  const [loadedImageSrc, setLoadedImageSrc] = useState(placeholder);
-  const imageRef = useRef<HTMLImageElement>(null);
-  useEffect(() => {
-    let observer: IntersectionObserver;
-    const element = imageRef.current!;
-
-    if (loadedImageSrc !== imageSrc) {
-      setLoadedImageSrc(placeholder);
-    }
-
-    if (element && loadedImageSrc !== imageSrc) {
-      if (IntersectionObserver) {
-        observer = new IntersectionObserver(
-          (entries) => {
-            entries.forEach((entry) => {
-              if (entry.intersectionRatio > 0 || entry.isIntersecting) {
-                setLoadedImageSrc(imageSrc);
-                observer.unobserve(element);
-              }
-            });
-          },
-          {
-            threshold: 0.01,
-            rootMargin: '50%',
-          },
-        );
-        observer.observe(element);
-      } else {
-        setLoadedImageSrc(imageSrc);
-      }
-    }
-
-    return () => {
-      if (observer && observer.unobserve) {
-        observer.unobserve(element);
-      }
-    };
-  }, [src, imageSrc, imageRef, loadedImageSrc]);
-
   return (
     <div className={clsx(styles.root, className)}>
-      <img ref={imageRef} alt={alt} className={styles.image} src={ResizedImageUrl(loadedImageSrc, 480)} />
+      {src && (
+        <img
+          alt={alt}
+          className={styles.image}
+          src={ResizedImageUrl(src, 480)}
+          onError={(event) => {
+            (event.target as HTMLImageElement).src = '/content/img/default-auction-preview.webp';
+          }}
+        />
+      )}
     </div>
   );
 };
