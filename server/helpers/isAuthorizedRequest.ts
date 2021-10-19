534079771a519bef901f8acf6550535600d24f76
Yauheni Shydlouski <yauheni.shydlouski@aterise.com>
2021-10-19 15:54:47 +0300

    Old metrics migration task

--- server/helpers/isAuthorizedRequest.ts
+++ server/helpers/isAuthorizedRequest.ts
@@ -1,8 +1,12 @@
 import { AppConfig } from '../config';
+import { AppLogger } from '../logger';
 
-export async function isAuthorizedRequest(req, res) {
-  if (req.body.key !== AppConfig.googleCloud.schedulerSecretKey) {
-    res.sendStatus(401).json({ message: 'UNAUTHORIZED' });
+export function isAuthorizedRequest(req, res) {
+  if (req.body?.key !== AppConfig.googleCloud.schedulerSecretKey) {
+    res.status(401).send('UNAUTHORIZED');
+    AppLogger.error(
+      `Unauthorized request received!\nPath: ${req.route.path};\nBody: ${JSON.stringify(req.body, null, 2)}`,
+    );
     return false;
   }
   return true;
