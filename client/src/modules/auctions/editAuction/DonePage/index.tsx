17fb7ee7c621f38a44e54690a61592562dcee238
Yauheni Shydlouski <yauheni.shydlouski@aterise.com>
2021-11-11 18:59:52 +0300

    webhooks logs

--- client/src/modules/auctions/editAuction/DonePage/index.tsx
+++ client/src/modules/auctions/editAuction/DonePage/index.tsx
@@ -47,7 +47,7 @@ const AuctionDonePage = () => {
             <p className="text-headline">Auction created.</p>
             <p className="text--body">
               Awesome! Let everyone know about this auction by using the link below and sharing it on your social
-              networks.
+              networks with the videos and photos you provided.
             </p>
             <div className="pt-3 pt-md-5 pb-3 pb-md-5 w-100">
               <CopiedText text={auctionLink} />
