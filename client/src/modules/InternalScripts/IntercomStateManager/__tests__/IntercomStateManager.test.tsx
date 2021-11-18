6df7fa98c196e8c9cb9de3d81e63a0eedae31dc7
Yauheni Shydlouski <yauheni.shydlouski@aterise.com>
2021-11-18 17:47:54 +0300

    tests for InternalScripts

--- client/src/modules/InternalScripts/IntercomStateManager/__tests__/IntercomStateManager.test.tsx
+++ client/src/modules/InternalScripts/IntercomStateManager/__tests__/IntercomStateManager.test.tsx
@@ -1,17 +1,54 @@
 import { mount, ReactWrapper } from 'enzyme';
+import { mocked } from 'ts-jest/utils';
+import { useAuth0 } from '@auth0/auth0-react';
 
 import IntercomStateManager from '../index';
+import { UserAccountContext } from 'src/components/helpers/UserAccountProvider/UserAccountContext';
+import { testAccount } from 'src/helpers/testHelpers/account';
 
-describe('Should render correctly "IntercomStateManager"', () => {
-  global.Intercom = jest.fn();
+describe('IntercomStateManager', () => {
+  jest.mock('@auth0/auth0-react');
+  const mockedUseAuth0 = mocked(useAuth0, true);
+
   let wrapper: ReactWrapper;
+  const OLD_ENV = process.env;
+
+  global.Intercom = jest.fn();
+
   beforeEach(() => {
-    wrapper = mount(<IntercomStateManager />);
+    process.env = { ...OLD_ENV };
   });
+
   afterEach(() => {
     jest.clearAllMocks();
+    process.env = OLD_ENV;
   });
-  it('component is defined', () => {
-    expect(wrapper).toHaveLength(1);
+
+  describe('with defined REACT_APP_INTERCOM_APP_ID', () => {
+    it('renders without errors', () => {
+      process.env.REACT_APP_INTERCOM_APP_ID = 'id';
+      const wrapper = mount(<IntercomStateManager />);
+      expect(wrapper).toHaveLength(1);
+    });
+  });
+
+  describe('with not defined REACT_APP_INTERCOM_APP_ID', () => {
+    it('renders without errors', () => {
+      process.env.REACT_APP_INTERCOM_APP_ID = null;
+      const wrapper = mount(<IntercomStateManager />);
+      expect(wrapper).toHaveLength(1);
+    });
+  });
+
+  describe('when user is logged in', () => {
+    it('renders without errors', () => {
+      process.env.REACT_APP_INTERCOM_APP_ID = 'id';
+      const wrapper = mount(
+        <UserAccountContext.Provider value={testAccount}>
+          <IntercomStateManager />
+        </UserAccountContext.Provider>,
+      );
+      expect(wrapper).toHaveLength(1);
+    });
   });
 });
--- client/src/modules/InternalScripts/NewRelicInitializer/__tests__/NewRelicInitializer.test.tsx
+++ client/src/modules/InternalScripts/NewRelicInitializer/__tests__/NewRelicInitializer.test.tsx
@@ -2,15 +2,58 @@ import { mount, ReactWrapper } from 'enzyme';
 
 import NewRelicInitializer from '../index';
 
-describe('Should render correctly "NewRelicInitializer"', () => {
+describe('NewRelicInitializer', () => {
   let wrapper: ReactWrapper;
+  const OLD_ENV = process.env;
+  global.NREUM = jest.fn();
+
   beforeEach(() => {
-    wrapper = mount(<NewRelicInitializer />);
+    process.env = { ...OLD_ENV };
   });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+    process.env = OLD_ENV;
+  });
+
   afterEach(() => {
     jest.clearAllMocks();
   });
-  it('component is defined', () => {
-    expect(wrapper).toHaveLength(1);
+
+  describe('without defined REACT_APP_USE_NEWRELIC', () => {
+    it('renders without errors', () => {
+      process.env.REACT_APP_USE_NEWRELIC = null;
+      wrapper = mount(<NewRelicInitializer />);
+      expect(wrapper).toHaveLength(1);
+    });
+  });
+  describe('when REACT_APP_USE_NEWRELIC=true', () => {
+    beforeEach(() => {
+      process.env.REACT_APP_USE_NEWRELIC = 'true';
+    });
+
+    describe('without defined REACT_APP_PLATFORM_URL', () => {
+      it('renders without errors', () => {
+        process.env.REACT_APP_PLATFORM_URL = null;
+        wrapper = mount(<NewRelicInitializer />);
+        expect(wrapper).toHaveLength(1);
+      });
+    });
+
+    describe('with defined REACT_APP_PLATFORM_URL', () => {
+      it('renders without errors', () => {
+        process.env.REACT_APP_PLATFORM_URL = 'https://dev.contrib.org/';
+        wrapper = mount(<NewRelicInitializer />);
+        expect(wrapper).toHaveLength(1);
+      });
+    });
+
+    describe('when defined REACT_APP_PLATFORM_URL is not suported', () => {
+      it('renders without errors', () => {
+        process.env.REACT_APP_PLATFORM_URL = 'http://localhost:3000/';
+        wrapper = mount(<NewRelicInitializer />);
+        expect(wrapper).toHaveLength(1);
+      });
+    });
   });
 });
