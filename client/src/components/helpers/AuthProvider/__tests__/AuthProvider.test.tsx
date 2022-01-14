b5875d877bbf6cb651286b636816e10dd6f53900
Yauheni Shydlouski <yauheni.shydlouski@aterise.com>
2022-01-14 18:07:49 +0300

    tests

--- client/src/components/helpers/AuthProvider/__tests__/AuthProvider.test.tsx
+++ client/src/components/helpers/AuthProvider/__tests__/AuthProvider.test.tsx
@@ -1,6 +1,9 @@
 import { mount, ReactWrapper } from 'enzyme';
 import { MemoryRouter } from 'react-router-dom';
+import { act } from 'react-dom/test-utils';
+
 import { AuthProvider } from 'src/components/helpers/AuthProvider/AuthProvider';
+
 const props = {
   children: <></>,
   apiUrl: 'testUrl',
@@ -8,28 +11,26 @@ const props = {
 
 describe('AuthProvider', () => {
   let wrapper: ReactWrapper;
-  beforeEach(() => {
-    wrapper = mount(
-      <MemoryRouter initialEntries={[`/`]}>
-        <AuthProvider {...props} />;
-      </MemoryRouter>,
-    );
+
+  beforeEach(async () => {
+    global.fetch = () =>
+      Promise.resolve({
+        json: () => Promise.resolve({ user: { id: 'testId' } }),
+      });
+
+    await act(async () => {
+      wrapper = mount(
+        <MemoryRouter initialEntries={[`/`]}>
+          <AuthProvider {...props} />;
+        </MemoryRouter>,
+      );
+
+      await new Promise((resolve) => setTimeout(resolve));
+      wrapper!.update();
+    });
   });
 
-  afterEach(() => {
-    jest.clearAllMocks();
-  });
   it('component is defined', () => {
     expect(wrapper).toHaveLength(1);
-  });
-
-  it('test', async () => {
-    // const a: any = {
-    //   json: jest.fn().mockResolvedValue({}),
-    // };
-    // jest.spyOn(global, 'fetch').mockResolvedValue(a);
-
-    await new Promise((resolve) => setTimeout(resolve));
-    wrapper.update();
   });
 });
