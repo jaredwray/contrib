e003346909d3747f72eca1fb248d6e0d4109cf25
Yauheni Shydlouski <yauheni.shydlouski@aterise.com>
2022-01-19 18:42:50 +0300

    auth logic in ContribApolloProvider fix

--- client/src/apollo/ContribApolloProvider/ContribApolloProvider.tsx
+++ client/src/apollo/ContribApolloProvider/ContribApolloProvider.tsx
@@ -2,6 +2,7 @@ import { useMemo } from 'react';
 
 import { ApolloClient, ApolloProvider, InMemoryCache, split } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
+import { onError } from '@apollo/client/link/error';
 import { WebSocketLink } from '@apollo/client/link/ws';
 import { getMainDefinition } from '@apollo/client/utilities';
 import { ApolloLink } from 'apollo-link';
@@ -34,10 +35,20 @@ export function ContribApolloProvider({ children }: PropTypes) {
       httpLink,
     );
 
+    const errorLink = onError(({ graphQLErrors }) => {
+      if (!graphQLErrors) return;
+
+      graphQLErrors.forEach(({ message }) => {
+        if (message.startsWith('Unauthorized')) {
+          window.location.href = `/log-in?returnURL=${window.location.pathname}`;
+        }
+      });
+    });
+
     const authLink = setContext(async (_, { headers }) => headers);
 
     return new ApolloClient({
-      link: ApolloLink.from([authLink as any, splitLink as any]) as any,
+      link: ApolloLink.from([authLink as any, errorLink as any, splitLink as any]) as any,
       cache: new InMemoryCache(),
       connectToDevTools: process.env.NODE_ENV === 'development',
       credentials: 'include',
