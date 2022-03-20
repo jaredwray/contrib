4fd171cebf46cab8eb7f3274715055579e8a6aca
Jason Baik <1562521+jasonbaik@users.noreply.github.com>
2022-03-20 09:10:04 -0700

    new footer (#927)
    * new footer
    
    * Update client/src/components/layouts/Layout/Footer/index.tsx
    
    Co-authored-by: Yauheni Shydlouski <madysh@users.noreply.github.com>
    
    * refactoring
    
    * Update client/src/components/layouts/Layout/Footer/index.tsx
    
    Co-authored-by: Yauheni Shydlouski <madysh@users.noreply.github.com>
    
    Co-authored-by: Yauheni Shydlouski <madysh@users.noreply.github.com>
    Co-authored-by: Yauheni Shydlouski <yauheni.shydlouski@aterise.com>

--- client/src/components/layouts/Layout/Footer/index.tsx
+++ client/src/components/layouts/Layout/Footer/index.tsx
@@ -4,41 +4,36 @@ import clsx from 'clsx';
 import { Container, Row, Col } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
-import Logo from 'src/assets/images/logo-with-text-white.svg';
+import Logo from 'src/assets/images/contrib-logo-vertical-negative.svg';
 
 import styles from './styles.module.scss';
 
 export default function Footer() {
   return (
-    <footer className={clsx('d-print-none ps-0 p-4 ps-sm-4', styles.footer)}>
+    <footer className={clsx('d-print-none px-0 py-4 p-md-4', styles.footer)}>
       <Container fluid="xxl">
         <Row>
-          <Col md className="p-0">
+          <Col className="p-0 text-center text-md-start" md="11">
+            <div className={clsx(styles.footerNav, 'text--body')}>
+              <Link to="/">About Us</Link>
+              &nbsp; &middot; &nbsp;
+              <Link to="/auctions">Auctions</Link>
+              &nbsp; &middot; &nbsp;
+              <Link to="/influencers">Influencers</Link>
+              &nbsp; &middot; &nbsp;
+              <Link to="/charities">Charities</Link>
+            </div>
+            <div className={clsx(styles.privacyPolicy, 'text-body-new')}>
+              <Link to="/privacy-policy">Privacy Policy</Link>
+              &nbsp; &middot; &nbsp;
+              <Link to="/terms">Terms</Link>
+              &nbsp; &middot; &nbsp; &copy;{new Date().getFullYear()} Contrib Inc.
+            </div>
+          </Col>
+          <Col className="p-0 text-center text-md-end pt-4 pt-md-0" md="1">
             <Link to="/">
               <img alt="Contrib" className={styles.logo} src={Logo} />
             </Link>
-          </Col>
-        </Row>
-        <Row className={clsx('py-4', styles.info)}>
-          <Col className="p-0 text-headline">Auction your items quickly and hassle free.</Col>
-        </Row>
-        <Row>
-          <Col md className="p-0 text-label text-all-cups">
-            Copyright {new Date().getFullYear()} Contrib Inc.
-          </Col>
-        </Row>
-        <Row>
-          <Col md className="p-0">
-            <div className="text-label text-all-cups">
-              <Link className={clsx('privacy', styles.link)} to="/privacy-policy">
-                Privacy
-              </Link>
-              &#160;and&#160;
-              <Link className={clsx('privacy', styles.link)} to="/terms">
-                Terms
-              </Link>
-              &#160;&gt;&gt;
-            </div>
           </Col>
         </Row>
       </Container>
