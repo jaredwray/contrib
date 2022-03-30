785bb5fb2749e650faf9fdfc6aa8da6cd62c1f41
Yauheni Shydlouski <yauheni.shydlouski@aterise.com>
2022-03-30 18:40:23 +0300

    Banner, new styles

--- client/src/modules/Home/Banner/index.tsx
+++ client/src/modules/Home/Banner/index.tsx
@@ -2,40 +2,22 @@ import React from 'react';
 
 import clsx from 'clsx';
 import { Container, Row, Col } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 import styles from './styles.module.scss';
 
 export default function Banner() {
   return (
-    <section className={styles.banner}>
-      <Container className={clsx(styles.homepageContainer, 'position-relative h-100')} fluid="xxl">
-        <div className={styles.wrapper}>
-          <Row>
-            <Col className="pt-5 pt-md-4 pt-lg-5 pb-3 pb-md-4 pb-lg-4 text-super" lg="8" xs="6">
-              Make An Impact
-            </Col>
-          </Row>
-          <Row>
-            <Col>
-              <div className={styles.separator} />
-            </Col>
-          </Row>
-          <Row>
-            <Col className={clsx(styles.message, 'pt-3 pb-3 text-super-headline text-capitalize')} lg="6">
-              Direct
-              <br />
-              influencer-to-fan
-              <br />
-              charity auctions
-            </Col>
-          </Row>
-          <Row>
-            <Col className={clsx(styles.signature, 'text-label text-all-cups position-absolute')} sm="6" xs="9">
-              Stefan Frei
-            </Col>
-          </Row>
-        </div>
-      </Container>
-    </section>
+    <Container fluid className={clsx(styles.wrapper, 'position-relative p-0')}>
+      <Row className="justify-content-md-center h-100">
+        <Col className={clsx(styles.text, 'text-center my-auto')} lg="6" md="8" xl="5">
+          CONTRIB is a fundraising platform that empowers influencers to engage with their fans and raise money for
+          charities.
+          <Link className={clsx(styles.link, 'text-label-new  d-block pt-1')} to="/">
+            Learn More &#8250;
+          </Link>
+        </Col>
+      </Row>
+    </Container>
   );
 }
--- client/src/modules/Home/__tests__/Banner.test.tsx
+++ client/src/modules/Home/__tests__/Banner.test.tsx
@@ -1,9 +1,15 @@
 import { render } from '@testing-library/react';
 
+import { BrowserRouter as Router } from 'react-router-dom';
+
 import Banner from '../Banner';
 
 jest.mock('src/components/modals/TermsConfirmationDialog', () => () => <></>);
 
 test('renders without crashing', () => {
-  render(<Banner />);
+  render(
+    <Router>
+      <Banner />
+    </Router>,
+  );
 });
