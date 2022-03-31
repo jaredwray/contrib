25d3030eb03544cfcb9768cf41ad6383fc3b2abf
Yauheni Shydlouski <yauheni.shydlouski@aterise.com>
2022-03-31 18:23:49 +0300

    ui fixes

--- client/src/modules/Home/Banner/index.tsx
+++ client/src/modules/Home/Banner/index.tsx
@@ -10,10 +10,8 @@ export default function Banner() {
     <Container fluid className={clsx(styles.wrapper, 'position-relative p-0')}>
       <Row className="justify-content-md-center h-100">
         <Col className={clsx(styles.text, 'text-center my-auto p-3')} md="8" xxl="6">
-          <p className="p-0">
-            <span className={styles.contribFontWeight}>CONTRIB</span> is a fundraising platform that empowers
-            influencers to engage with their fans and raise money for charities.
-          </p>
+          <span className={styles.contribFontWeight}>CONTRIB</span> is a fundraising platform that empowers influencers
+          to engage with their fans and raise money for charities.
         </Col>
       </Row>
     </Container>
