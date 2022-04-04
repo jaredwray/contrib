d98450b2027b52c868efa48ff92559675c276141
Jason Baik <1562521+jasonbaik@users.noreply.github.com>
2022-04-04 14:23:34 -0700

    metrics banner style updates (#967)
    * style updates
    
    * style updates
    
    * styling for slider cards
    
    * more styling
    
    * styling
    
    * fix eslint and add styling
    
    * styling
    
    * styling

--- client/src/modules/Home/Status/Badges/TotalAmount/index.tsx
+++ client/src/modules/Home/Status/Badges/TotalAmount/index.tsx
@@ -16,14 +16,14 @@ interface PropTypes {
 
 export const TotalAmount = ({ title, secondValue, firstValue, icon, link }: PropTypes): ReactElement => {
   return (
-    <Row className="text-sm p-2">
+    <Row className="text-sm pb-2">
       <Col className="d-flex justify-content-center p-0 flex-column text-center">
         <div className="pb-2">
           <Image className={clsx(styles.icon, 'pt-1')} src={icon} />
         </div>
         <div className={clsx(styles.wrapper, 'p-0 text-center')}>
           <div className={clsx(styles.title, 'text-all-cups')}>{title}</div>
-          <div className={clsx(styles.firstValueWrapper, 'text-truncate m-auto')}>
+          <div className={clsx(styles.firstValueWrapper, 'text-truncate m-auto pt-1')}>
             {link ? (
               <Link className={styles.link} to={link}>
                 {firstValue}
--- client/src/modules/Home/Status/index.tsx
+++ client/src/modules/Home/Status/index.tsx
@@ -9,18 +9,18 @@ import styles from './styles.module.scss';
 export default function Status() {
   return (
     <Container fluid className={styles.root}>
-      <Container className="p-0" fluid="xxl">
+      <Container className="pt-2" fluid="xxl">
         <Row className="pt-4">
-          <Col className="text-center px-0 pb-2">
+          <Col className="text-center px-0 pb-4 py-2">
             <div className={clsx(styles.title, 'm-auto pb-2 pb-md-0')}>
-              Make an impact.
+              Make an impact!
               <span className={clsx(styles.subtitle, 'd-inline-block')}>
-                &nbsp; Direct Influencer-To-Fan Charity Auctions.
+                &nbsp; Direct <span className={styles.italicSubtitle}>Influencer-To-Fan</span> Charity Auctions.
               </span>
             </div>
           </Col>
         </Row>
-        <Row className="pt-0 justify-content-center">
+        <Row className="d-flex flex-row justify-content-center pt-0">
           <Badges />
         </Row>
       </Container>
