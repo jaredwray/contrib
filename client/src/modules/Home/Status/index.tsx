bc28609179203a6166c66581627762ecf61fdb98
Jason Baik <baikjason@gmail.com>
2022-04-12 09:45:53 -0700

    updates

--- client/src/modules/Home/Status/index.tsx
+++ client/src/modules/Home/Status/index.tsx
@@ -8,7 +8,7 @@ import styles from './styles.module.scss';
 
 export default function Status() {
   return (
-    <Container fluid className={clsx(styles.root, 'p-0')}>
+    <Container fluid className={clsx(styles.root, 'p-0 pt-2')}>
       <Container className="p-0" fluid="xxl">
         <Row className="pt-0 mb-4 px-0">
           <Badges />
