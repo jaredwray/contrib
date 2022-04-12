31f9fad24a8db0773acb65404ae7702fdd50cb04
Jason Baik <baikjason@gmail.com>
2022-04-12 10:10:33 -0700

    styling

--- client/src/modules/Home/Banner/index.tsx
+++ client/src/modules/Home/Banner/index.tsx
@@ -10,8 +10,8 @@ export default function Banner() {
   return (
     <Container fluid className={clsx(styles.wrapper, 'position-relative p-0 pb-4')}>
       <Row className="justify-content-md-center h-100">
-        <Col lg="8" md="12" xxl="6">
-          <div className={clsx(styles.text, 'text-center my-auto p-4')}>
+        <Col lg="10" md="12" xxl="6">
+          <div className={clsx(styles.text, 'text-center my-auto p-4 py-md-5')}>
             <span className={styles.contribFontWeight}>CONTRIB</span> is a fundraising platform that empowers
             influencers to engage with their fans and raise money for charities.
           </div>
--- client/src/modules/Home/EndingSoon/index.tsx
+++ client/src/modules/Home/EndingSoon/index.tsx
@@ -25,9 +25,9 @@ export default function EndingSoon() {
   const items = data.auctions.items.map((auction: Auction, i: number) => <AuctionCard key={i} auction={auction} />);
 
   return (
-    <Container fluid className={clsx(styles.root, 'overflow-hidden text-left p-4 pb-5')}>
+    <Container fluid className={clsx(styles.root, 'overflow-hidden text-left p-4 pt-md-5 pb-5')}>
       <Container className={clsx('p-0', !items.length && 'text-center')} fluid="xxl">
-        <Row className="p-0 py-2">
+        <Row className="p-0 pb-4">
           <span className={clsx(styles.subtitle, 'p-0 text-center text-md-start')}>
             Direct <span className={styles.italicSubtitle}>Influencer-To-Fan</span> Charity Auctions
           </span>
