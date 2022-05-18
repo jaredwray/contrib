f2ca6920c98fc40ed1b1afa3ce70fc11d6a54fba
Yauheni Shydlouski <madysh@users.noreply.github.com>
2022-05-18 19:27:20 +0300

    improvements for invitations (#1025)
    Co-authored-by: Yauheni Shydlouski <yauheni.shydlouski@aterise.com>

--- client/src/components/forms/inputs/PhoneInput2/index.tsx
+++ client/src/components/forms/inputs/PhoneInput2/index.tsx
@@ -1,10 +1,10 @@
-import { FC } from 'react';
+import { FC, useCallback, useState } from 'react';
 
 import clsx from 'clsx';
 import { Field } from 'react-final-form';
 import PhoneInput from 'react-phone-input-2';
 
-import { ALLOWED_COUNTRY_NAME } from 'src/types/Country';
+import { ALLOWED_COUNTRY_NAME, Country } from 'src/types/Country';
 
 import styles from './styles.module.scss';
 
@@ -15,32 +15,46 @@ interface Props {
 }
 
 const USPhoneInput: FC<Props> = ({ className, placeholder, disabled = false }: Props) => {
+  const [inputValue, setInputValue] = useState('');
+
+  const onClick = useCallback(
+    (event: any, data: any) => {
+      !inputValue && setInputValue('+1');
+    },
+    [inputValue, setInputValue],
+  );
   return (
     <Field name="phoneNumber">
-      {({ input }) => (
-        <PhoneInput
-          containerClass={clsx(styles.input, className)}
-          copyNumbersOnly={false}
-          disabled={disabled}
-          inputProps={{
-            required: true,
-            name: 'phoneNumber',
-            country: 'us',
-          }}
-          isValid={(value: string, country: any): any => {
-            if (value && country.name !== ALLOWED_COUNTRY_NAME)
-              return (
-                <p className={clsx(styles.error, 'text-label error-message position-absolute')}>
-                  Must be a valid US phone number
-                </p>
-              );
-            return true;
-          }}
-          placeholder={placeholder}
-          specialLabel=""
-          {...input}
-        />
-      )}
+      {({ input }) => {
+        const { value, onChange, ...inputProps } = input;
+        return (
+          <PhoneInput
+            containerClass={clsx(styles.input, className)}
+            copyNumbersOnly={false}
+            disabled={disabled}
+            inputProps={{
+              required: true,
+              name: 'phoneNumber',
+              country: 'us',
+            }}
+            isValid={(value: string, country: any): any => {
+              if (value && country.name !== ALLOWED_COUNTRY_NAME)
+                return (
+                  <p className={clsx(styles.error, 'text-label error-message position-absolute')}>
+                    Must be a valid US phone number
+                  </p>
+                );
+              return true;
+            }}
+            placeholder={placeholder}
+            specialLabel=""
+            value={inputValue}
+            onChange={(value: string, country: Country) => setInputValue(value)}
+            onClick={onClick}
+            {...inputProps}
+          />
+        );
+      }}
     </Field>
   );
 };
