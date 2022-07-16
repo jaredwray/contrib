2e56912bac2604a95052f7a4317a83444f681619
Jason Baik <1562521+jasonbaik@users.noreply.github.com>
2022-07-16 13:18:43 -0700

    update home page stat bar styling (#1067)

--- client/src/modules/Home/Status/Badges/index.tsx
+++ client/src/modules/Home/Status/Badges/index.tsx
@@ -8,9 +8,6 @@ import { Container, Col, Row } from 'react-bootstrap';
 import { TotalRaisedAmountQuery } from 'src/apollo/queries/auctions';
 import { TopCharityQuery } from 'src/apollo/queries/charities';
 import { TopEarnedInfluencerQuery } from 'src/apollo/queries/influencers';
-import AwardIcon from 'src/assets/images/award.svg';
-import TopCharityIcon from 'src/assets/images/top-charity.svg';
-import TrophyIcon from 'src/assets/images/trophy.svg';
 
 import styles from './styles.module.scss';
 import { TotalAmount } from './TotalAmount';
@@ -25,35 +22,35 @@ export const Badges = (): ReactElement => {
   const topCharity = topCarityData?.topCharity;
 
   return (
-    <Container className={clsx(styles.badgesWrapper, 'd-flex flex-row justify-content-center py-2')}>
+    <Container className={clsx(styles.badgesWrapper, 'd-flex flex-row justify-content-center')}>
       <Row>
-        <Col className={clsx(styles.item, 'text-center p-4 py-md-2')} md="auto">
+        <Col className={clsx(styles.item, 'text-center p-4')} md="auto">
           <TotalAmount
-            firstValue="ALL TIME"
-            icon={TrophyIcon}
-            secondValue={totalRaised && Dinero({ amount: totalRaised }).toFormat('$0,0')}
+            firstValue={totalRaised && Dinero({ amount: totalRaised }).toFormat('$0,0')}
+            secondValue="ALL TIME"
             title="total raised"
           />
         </Col>
-        <Col className={clsx(styles.item, styles.topEarner, 'p-4 py-md-2')} md="auto">
+        <Col className={clsx(styles.item, styles.topEarner, 'p-4')} md="auto">
           <TotalAmount
             avatar={topEarned?.avatarUrl}
-            firstValue={topEarned?.name}
-            icon={AwardIcon}
+            firstValue={topEarned && Dinero(topEarned.totalRaisedAmount).toFormat('$0,0')}
             link={topEarned && `/profiles/${topEarned.id}`}
-            secondValue={topEarned && Dinero(topEarned.totalRaisedAmount).toFormat('$0,0')}
+            secondValue={topEarned?.name}
             title="top earner"
           />
         </Col>
-        <Col className={clsx(styles.item, 'p-4 py-md-2')} md="auto">
+        <Col className={clsx(styles.item, 'p-4')} md="auto">
           <TotalAmount
             avatar={topCharity?.avatarUrl}
-            firstValue={topCharity?.name}
-            icon={TopCharityIcon}
+            firstValue={topCharity && Dinero(topCharity.totalRaisedAmount).toFormat('$0,0')}
             link={topCharity && `/charity/${topCharity.semanticId || topCharity.id}`}
-            secondValue={topCharity && Dinero(topCharity.totalRaisedAmount).toFormat('$0,0')}
+            secondValue={topCharity?.name}
             title="top charity"
           />
+        </Col>
+        <Col className={clsx(styles.item, 'text-center p-4')} md="auto">
+          <TotalAmount firstValue="$5,000" secondValue="TOTAL MATCHED" title="CORPORATE SPONSORS" />
         </Col>
       </Row>
     </Container>
