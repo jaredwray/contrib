d3ace90a762a9d9fd47883f4795df402a12aa9ad
Jason Baik <baikjason@gmail.com>
2022-09-07 11:04:41 -0700

    cleanup code

--- client/src/modules/auctions/editAuction/AuctionAttachmentsPage/UploadingDropzone/index.tsx
+++ client/src/modules/auctions/editAuction/AuctionAttachmentsPage/UploadingDropzone/index.tsx
@@ -152,9 +152,9 @@ const UploadingDropzone: FC<Props> = ({
           />
         ))}
         <div className={styles.filePreviewWrapper}>
-          {attachments.loading.map((file: File, index: number) => {
-            return <FilePreview key={index} file={file} />;
-          })}
+          {attachments.loading.map((file: File, index: number) => (
+            <FilePreview key={index} file={file} />
+          ))}
         </div>
       </div>
       <div {...getRootProps({ className: styles.dropzone })}>
