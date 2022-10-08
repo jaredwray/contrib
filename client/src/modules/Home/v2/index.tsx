3012ba1ee318019500197abe77236cbe639ce32c
Jason Baik <baikjason@gmail.com>
2022-10-08 11:48:20 -0700

    refactor ending soon query for charities view

--- client/src/modules/Home/v2/index.tsx
+++ client/src/modules/Home/v2/index.tsx
@@ -17,22 +17,27 @@ export default function HomePage() {
       size: 25,
       orderBy: 'ENDING_SOON',
       filters: {
-        status: [AuctionStatus.ACTIVE],
+        status: [AuctionStatus.ACTIVE, AuctionStatus.SETTLED, AuctionStatus.SOLD],
       },
     },
   });
 
   if (loading || error) return null;
 
-  const charities = new Set();
-  data.auctions.items.forEach((i: any) => charities.add({ id: i.charity.id, name: i.charity.name }));
+  let charities = new Set();
+
+  data.auctions.items.forEach(
+    (i: any) =>
+      !charities.has(JSON.stringify(i)) && charities.add(JSON.stringify({ id: i.charity.id, name: i.charity.name })),
+  );
+
+  charities = new Set([...charities].map((o: any) => JSON.parse(o)));
   const charityItems = [...charities].map((charity: any, i: number) => <EndingSoon key={i} charity={charity} />);
 
   return (
     <Layout>
       <Banner />
       {charityItems}
-      {!isAuthenticated && <HowTo />}
     </Layout>
   );
 }
